0696ba0f42e6d5d47f2933417ce3c975f698de68
kojotak <tomas.beneda@gmail.com>
2021-03-27 13:49:40 +0100

    section 4 ... marketing module

---
+++ project2/marketing/config/webpack.dev.js
@@ -0,0 +1,20 @@
+const { merge } = require ('webpack-merge'); //umi mergovat webpack configurace
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+
+const devConfig ={
+    mode: 'development',
+    devServer:{
+        port: 8081,
+        historyApiFallback:{
+            index: 'index.html',
+        }
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./public/index.html'
+        })
+    ]
+};
+
+module.exports = merge(commonConfig, devConfig);
