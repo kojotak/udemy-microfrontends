5b818a30a26774faa5e3e3fb27688352f5f20eb8
kojotak <tomas.beneda@gmail.com>
2021-03-29 21:30:13 +0200

    section 9 ... added custom CSS class name prefix to avoid collisions

--- mfp/container/src/App.js
+++ mfp/container/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
+import {StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co' //co=container
+});
 
 export default () => {
     return(
-    <BrowserRouter>
+    <BrowserRouter generateClassName={generateClassName}>
         <div>
             <Header />
             <hr/>
