34a0582d631e51ce72aee396fb989c695bd65a0d
kojotak <tomas.beneda@gmail.com>
2021-04-03 14:26:22 +0200

    section 11... added lazy loading

--- mfp/container/src/App.js
+++ mfp/container/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
-import AuthApp from './components/Auth';
+import React, {lazy, Suspense} from 'react';
+
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import {StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const MarketingLazy = lazy( ()=> import('./components/MarketingApp'));
+const AuthLazy = lazy( ()=> import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
     productionPrefix: 'co' //co=container
@@ -16,10 +18,12 @@ export default () => {
             <div>
                 <Header />
                 <hr/>
-                <Switch>
-                    <Route path="/auth" component={AuthApp} />
-                    <Route path="/" component={MarketingApp} />
-                </Switch>
+                <Suspense fallback={<div>loading...</div>}>
+                    <Switch>
+                        <Route path="/auth" component={AuthLazy} />
+                        <Route path="/" component={MarketingLazy} />
+                    </Switch>
+                </Suspense>
             </div>
         </StylesProvider>
     </BrowserRouter>)
